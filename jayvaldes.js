efe527d12fca713ab5e075bfdfa7aa2e2574426c
Paul Borawski <paulborawski14@gmail.com>
2017-04-18 11:45:32 -0700

    basic webiste

---
+++ jayvaldes.js
@@ -0,0 +1,16 @@
+var port = 4007;
+var app = require('express')();
+
+app.use(require('compression')());
+app.use(require('body-parser').json());
+app.use(require('body-parser').urlencoded({ extended:true }));
+app.use(require('express').static('public'));
+
+app.get('/', function (req, res) {
+  res.send('jayvaldes');
+});
+
+
+require('http').createServer(app).listen(port,function(){
+  console.log('server is running you beeezy, on port', port)
+});
---
+++ public/jay.js
@@ -0,0 +1,1 @@
+document.querySelector('.jay1').style.fontSize = '100px'
